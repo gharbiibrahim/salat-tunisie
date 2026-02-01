1b5d1415a30d4c2950d9619dd59419ed5f79da80
gharbiibrahim <ielgharbi@gmail.com>
2026-02-01 22:29:39 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'salat-tunisie-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './css/style.css',
+    './js/app.js',
+    './js/calculator.js',
+    './manifest.json',
+    './assets/icon-192.png',
+    './assets/icon-512.png',
+    './assets/adhan.mp3',
+    './assets/adhan_fajr.mp3'
+];
+
+// Install Event: Cache Files
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+// Activate Event: Cleanup Old Caches
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) return caches.delete(key);
+                })
+            );
+        })
+    );
+});
+
+// Fetch Event: Serve from Cache
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => {
+            return res || fetch(e.request);
+        })
+    );
+});
